4f3da44ad7ea94a7a0a577a80447197792590dd2
prashant5harma <mr.prashansharma@gmail.com>
2017-04-15 19:56:42 -0400

    added question 3 and 5
    added question 3 and 5

--- log_analysis.py
+++ log_analysis.py
@@ -12,3 +12,19 @@ output_text_file=sys.argv[2]
 counts=sc.textFile(input_text_file).flatMap(lambda x: x.split()).map(lambda x: (x,1)).reduceByKey(lambda x,y: x+y)
 
 counts.saveAsTextFile(output_text_file)
+
+
+
+
+Q 3
+counts=sc.textFile("testt.txt").map(lambda x: (1, x.split()[::-1])).map(lambda x:(x[0],x[1][0])).map(lambda x:x[1])
+
+
+Q 5
+
+from pyspark.sql import Row
+from pyspark.sql.functions import *
+text_file = sc.textFile("testt.txt")
+df = text_file.map(lambda r: Row(r)).toDF(["line"])
+df.show()
+df.filter(lower(df['line']).rlike("qwrwret") ).count()
